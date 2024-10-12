3a96bdd85afab68de77f6b603826069e76cd1e15
idi <idriscreate@gmail.com>
2024-10-12 14:18:39 -0700

    yes

--- src/components/MissionVision.jsx
+++ src/components/MissionVision.jsx
@@ -6,7 +6,7 @@ const MissionVision = ({title, content}) => {
         <div className="p-2 bg-sky-300 rounded-full absolute top-1/3 left-1/3 animate-bounce"></div>
         <div className="p-2 bg-sky-300 rounded-full absolute top-3/4 left-3/4 animate-bounce"></div>
         <div className="p-2 bg-sky-300 rounded-full absolute top-1/4 left-1/4 animate-bounce"></div>
-        <div className="bg-transparent backdrop-blur-sm shadow-md hover:scale-105 shadow-sky-800 p-2 sm:p-4 rounded-md md:rounded-lg mt-3 sm:mt-4 min-h-fit md:min-h-40">
+        <div className="bg-transparent backdrop-blur-sm shadow-md sm:hover:scale-105 shadow-sky-800 p-2 sm:p-4 rounded-md md:rounded-lg mt-3 sm:mt-4 min-h-fit md:min-h-40">
             <h3 className="bg-clip-text text-transparent bg-gradient-to-br from-blue-800 via-sky-400 to-blue-800 font-semibold sm:font-bold text-sm md:text-base mb-2">{title}</h3>
             <p className="text-sky-200 text-sm md:text-base">{content}</p>
         </div>
