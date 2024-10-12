bb99884936c89798f04ddad7703aacf448ad9ec9
idi <idriscreate@gmail.com>
2024-10-12 00:19:53 -0700

    fixes

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -10,16 +10,16 @@ const logos = [
 
 const Slider = () => {
   return (
-    <div className="overflow-hidden w-full py-4">
+    <div className="overflow-hidden w-full py-4 rounded-l-full rounded-r-full">
       <div className="flex w-max animate-scroll">
         {logos.map((logo, index) => (
           <div key={index} className="mx-4">
-            <img src={logo} alt={`logo-${index}`} className="w-36" />
+            <img src={logo} alt={`logo-${index}`} className="w-24 h-24 sm:w-28 sm:h-28 md:w-36 md:h-36 rounded-full" />
           </div>
         ))}
         {logos.map((logo, index) => (
           <div key={index + logos.length} className="mx-4">
-            <img src={logo} alt={`logo-${index}`} className="w-36" />
+            <img src={logo} alt={`logo-${index}`} className="w-24 h-24 sm:w-28 sm:h-28 md:w-36 md:h-36 rounded-full" />
           </div>
         ))}
       </div>
