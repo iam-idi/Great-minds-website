ca6dfa29ab8c9f6ce59c21c116d3d1760266ea58
idi <idriscreate@gmail.com>
2024-10-13 01:35:39 -0700

    added partners

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,11 +1,21 @@
-
+import bluehouse from '../assets/images/bluehouse.png';
+import lumina from '../assets/images/lumina.png';
+import loginallabs from '../assets/images/loginallabs.png';
+import nhub from '../assets/images/nhub.png';
+import kchub from '../assets/images/kchub.png';
+import decentdesigns from '../assets/images/decentdesigns.png';
+import stl from '../assets/images/stl.png';
+import mehub from '../assets/images/mehub.png';
 
 const logos = [
-  'https://via.placeholder.com/150',
-  'https://via.placeholder.com/150',
-  'https://via.placeholder.com/150',
-  'https://via.placeholder.com/150',
-  'https://via.placeholder.com/150',
+  bluehouse,
+  lumina,
+  nhub,
+  loginallabs,
+  kchub,
+  decentdesigns,
+  stl,
+  mehub, 
 ];
 
 const Slider = () => {
@@ -14,12 +24,12 @@ const Slider = () => {
       <div className="flex w-max animate-scroll">
         {logos.map((logo, index) => (
           <div key={index} className="mx-4">
-            <img src={logo} alt={`logo-${index}`} className="w-24 h-24 sm:w-28 sm:h-28 md:w-36 md:h-36 rounded-full" />
+            <img src={logo} alt={`logo-${index}`} className="bg-slate-400  border-sky-700 w-24 h-24 sm:w-28 sm:h-28 md:w-36 md:h-36 rounded-full" />
           </div>
         ))}
         {logos.map((logo, index) => (
           <div key={index + logos.length} className="mx-4">
-            <img src={logo} alt={`logo-${index}`} className="w-24 h-24 sm:w-28 sm:h-28 md:w-36 md:h-36 rounded-full" />
+            <img src={logo} alt={`logo-${index}`} className="bg-slate-400 border-sky-700 w-24 h-24 sm:w-28 sm:h-28 md:w-36 md:h-36 rounded-full" />
           </div>
         ))}
       </div>
