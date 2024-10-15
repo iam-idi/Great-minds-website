76275b81579c382a22a20187d0979a7ae2aeb31a
idi <idriscreate@gmail.com>
2024-10-15 23:29:04 +0100

    done with team

---
+++ data/team.js
@@ -0,0 +1,53 @@
+import logo from '../src/assets/images/logo.png';
+
+const teamData = [
+    {
+        name: 'Aliyu Haidar',
+        position: 'Founder/CEO',
+        skill: 'Cyber-Security & Data Analyst',
+        photo: logo,
+        links: {
+            twitter:'',
+            linkedin:'',
+            website:''
+        }
+    },
+
+    {
+        name: "Idris Mua'zu",
+        position: 'Software Engineer',
+        skill: 'Web, AI & ML',
+        photo: logo,
+        links: {
+            twitter:'',
+            linkedin:'',
+            website:'idris.vercel.app'
+        }
+    },
+
+    {
+        name: 'Abdulbasit Hussein',
+        position: 'Software Engineer',
+        skill: 'Web & Desktop Apps',
+        photo: logo,
+        links: {
+            twitter:'',
+            linkedin:'',
+            website:''
+        }
+    },
+
+    {
+        name: 'Naseer Abdulhaleem',
+        position: 'Software Engineer',
+        skill: 'Web & Cloud Computing',
+        photo: logo,
+        links: {
+            twitter:'',
+            linkedin:'',
+            website:''
+        }
+    },
+]
+
+export default teamData;
--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,10 +1,32 @@
-
-
+import { Link } from "react-router-dom";
+import teamData from "../../data/team";
+import { FaLink, FaLinkedin, FaTwitter } from "react-icons/fa";
 const Team = () => {
   return (
     <>
       <h1 className="text-xl xs:text-2xl sm:text-3xl bg-clip-text text-transparent bg-gradient-to-r from-blue-800 via-sky-400 to-blue-800 font-bold xs:font-extrabold text-center mt-4 sm:mt-6 md:mt-10 mb-4 sm:mb-6">Meet Our Team</h1>
       <div className="w-4/5 sm:w-full border-2 border-slate-800 mx-auto mt-0 mb-6 sm:mb-8 md:mb-10"></div>
+
+      <div className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 auto-rows-fr gap-2 sm:gap-3 mb-8 xs:mb-10 md:mb-14">
+        {teamData.map((team, index) => (
+
+          <div key={index} className="mb-1 sm:mb-2 text-center">
+            
+            <img src={team.photo} alt='' className="w-24 xs:h-24 md:w-28 md:h-28 lg:w-32 lg:h-32 my-0 mx-auto rounded-full" />
+            <h2 className="break-words text-sm xs:font-semibold xl:text-base text-sky-100">{team.name}</h2>
+            <h3 className="break-words text-sm xl:text-base text-slate-300">{team.position}</h3>
+
+            <div className="flex gap-2 md:gap-4 xl:gap-6 justify-center items-center text-slate-400 font-semibold md:font-bold text-base xl:text-lg mt-2">
+              <Link to={team.links.twitter} className="hover:scale-125"><FaTwitter /></Link>
+              <Link to={team.links.linkedin} className="hover:scale-125"><FaLinkedin /></Link>
+              <Link to={team.links.website} className="hover:scale-125"><FaLink /></Link>
+            </div>
+
+          </div>
+
+        ))}
+      </div>
+
     </>
   )
 }
